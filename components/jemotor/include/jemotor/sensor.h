a5a46d2365c1bda566540b035d6f339af065868c
James Aguilar <aguilar.james@gmail.com>
2025-01-22 22:24:20 -0500

    Switch sensor.h over to floats.
    Probably makes it so the repo doesn't compile any mroe but yolo.

--- components/jemotor/include/jemotor/sensor.h
+++ components/jemotor/include/jemotor/sensor.h
@@ -8,14 +8,14 @@ class Sensor {
   virtual ~Sensor() {}
 
   // Returns the current angle of the in decidegrees, between 0 and 3600.
-  virtual SQ15x16 angle() = 0;
+  virtual float angle() = 0;
 
   // Returns the rate of rotation in degrees/sec during the last sensing
   // period. Positive is clockwise.
-  virtual SQ15x16 rate() = 0;
+  virtual float rate() = 0;
 
   // Sets the current angle. Calling this without arguments zeroes the sensor.
-  virtual void SetAngle(SQ15x16 angle = 0) = 0;
+  virtual void SetAngle(float angle = 0) = 0;
 };
 
 }  // namespace jemotor
